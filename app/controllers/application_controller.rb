9204d87763a2e8b33a3a2d0fdf8c86c0d60445d9
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-20 22:35:04 -0600

    added user registeration, login,logout features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  # SESSIONS SYNTAX!!
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 end
