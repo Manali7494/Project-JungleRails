ad5f285dd0a99fa66880c70c9004923e8fa00952
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-22 14:45:34 -0600

    added user display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,16 @@
-class ProductsController < ApplicationController
+# frozen_string_literal: true
 
+class ProductsController < ApplicationController
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
+    
     @product = Product.find params[:id]
     product_id = @product.id
+    @users = User.all
     @reviews = ProductReview.where("product_id = #{product_id}")
+    
   end
-
 end
